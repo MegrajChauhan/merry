e9d29ec2a0963fac9d1bf36fa5e17a715e6adeee
Aryan Chauhan <134856674+MegrajChauhan@users.noreply.github.com>
2025-11-21 07:42:20 +0545

    Update merry_acceptor.h

--- merry/core/acceptor/merry_acceptor.h
+++ merry/core/acceptor/merry_acceptor.h
@@ -4,14 +4,17 @@
 #include <merry_types.h>
 #include <merry_utils.h>
 #include <merry_platform.h>
-#include <merry_file.h>
 #include <merry_operations.h>
+#include <merry_list.h>
+#include <merry_protector.h>
+#include <merry_client_list.h>
 #include <stdlib.h>
 
 typedef struct MerryAcceptor MerryAcceptor;
 
 struct MerryAcceptor {
-	MerryFile file;
+	mmutex_t *shared_lock;
+ MerryClientList **list;
 	msocket_t listening_socket;
 	msize_t accepted_connections; // the number of accepted connections
 	msize_t connection_limit;
@@ -19,6 +22,10 @@ struct MerryAcceptor {
 	msize_t STOP; // to close the acceptor
 };
 
-mresult_t merry_acceptor_init(MerryAcceptor **acceptor, );
+mresult_t merry_acceptor_init(MerryAcceptor **acceptor, mmutex_t *shared_lock, MerryClientList **list, msocket_t sock);
+
+void merry_acceptor_destroy(MerryAcceptor *acceptor);
+
+_THRET_T_ merry_acceptor_run(mptr_t a);
 
 #endif
