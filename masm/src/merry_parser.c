39d1532f932f932369af359f4bfd85c775121146
Megraj Chauhan <megrajchauhan123@gmail.com>
2026-02-13 23:27:21 +0545

    variable definitions

--- masm/src/merry_parser.c
+++ masm/src/merry_parser.c
@@ -2,6 +2,20 @@
 
 _MERRY_DEFINE_DYNAMIC_LIST_(Node, Node*);
 
+_MERRY_INTERNAL_ datatype_t token_to_datatype(token_t type) {
+	switch (type) {
+		case TOK_DB:
+		  return TYPE_BYTE;
+		case TOK_DW:
+		  return TYPE_WORD;
+		case TOK_DD:
+		  return TYPE_DWORD;
+		case TOK_DQ:
+		  return TYPE_QWORD;
+	}
+	return TYPE_QWORD; // this is nonsensical really as all calls to token_to_datatype will be valid
+}
+
 _MERRY_INTERNAL_ token_t identify_keyword(Parser *p, Token *tok) {
     // using switch-statements instead
     msize_t len = (msize_t)(tok->end - tok->start);
@@ -31,11 +45,20 @@ _MERRY_INTERNAL_ token_t identify_keyword(Parser *p, Token *tok) {
 	return TOK_ERR;
 }
 
-// TODO!!!!
-_MERRY_INTERNAL_ mbool_t handle_variable_defins(Parser *p, token_t ) {
-	mdatatype_t type;
+_MERRY_INTERNAL_ mbool_t handle_variable_defins(Parser *p, token_t tok) {
+	datatype_t type;
     Token var_name = lexer_next(p->l);
-    if (var_name.type != TOK_IDENTIFIER) 
+    if (var_name.type != TOK_IDENTIFIER) {
+    	MERR("[LINE: %zu]: Expected an identifier after variable definition.", var_name.lnum);
+    	return mfalse;
+    }
+    type = token_to_datatype(tok);
+    node_t node_type;
+    if (!parse_expr(p->expr, p->l)) {
+    	return mfalse;
+    }
+    // TODO: Continue from here!!
+    return mtrue;
 }
 
 
